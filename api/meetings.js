74c4b47efeb1acb59f1d66ebdd49b21ab3a1a834
aditya <aditya.dc0497@gmail.com>
2024-12-11 19:20:37 +0530

    db connections with firebase:3

--- api/meetings.js
+++ api/meetings.js
@@ -1,13 +1,15 @@
 const admin = require('firebase-admin');
 
 // Initialize Firebase Admin SDK with the service account
-admin.initializeApp({
-  credential: admin.credential.cert({
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
     project_id: process.env.FIREBASE_PROJECT_ID,
     private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
   }),
 });
+}
 
 const db = admin.firestore();
 
